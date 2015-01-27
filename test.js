7e5126bce8f08499e9c6978588425b742b17e1cc
Matt Brennan <mattyb1000@gmail.com>
2015-01-27 13:29:56 +0000

    more tests

--- test.js
+++ test.js
@@ -20,6 +20,37 @@ methods.forEach(function(method) {
 
 			wrapped({method: method});
 			expect(handler).was.called();
+		},
+
+		'args and return value': function() {
+			var handler = sinon.stub().returns('foo');
+			var wrapped = wrapper(handler);
+			var req = {method: method};
+
+			expect(wrapped(req)).to.be('foo');
+			expect(handler).was.calledWith(req);
+		},
+
+		'getMethod': function() {
+			var wrapper = μ[camelCase(method) + '_'](function getMethod_() {
+				return method;
+			});
+			var handler = sinon.spy();
+			var wrapped = wrapper(handler);
+
+			wrapped({method: method});
+			expect(handler).was.called();
+		},
+
+		'withGetMethod': function() {
+			var wrapper = μ.withGetMethod(function getMethod_() {
+				return method;
+			})[camelCase(method)];
+			var handler = sinon.spy();
+			var wrapped = wrapper(handler);
+
+			wrapped({method: method});
+			expect(handler).was.called();
 		}
 	};
 });
